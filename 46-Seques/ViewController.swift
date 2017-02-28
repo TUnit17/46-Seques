4e23444f9bb47e4f02f0476f300d6547ba264443
TUnit17 <thuang177@gmail.com>
2017-02-27 18:29:10 -0800

    Revert "First commit"
    This reverts commit 26d165963d4769d45d225210b341a1fee2712dcf.
    
    # Conflicts:
    #	46-Seques/PlaySongVCViewController.swift

--- 46-Seques/ViewController.swift
+++ 46-Seques/ViewController.swift
@@ -6,29 +6,20 @@
 //  Copyright © 2017 Timothy Huang. All rights reserved.
 //
 
-/**
- 
- 
- 
- **/
-
-
 import UIKit
 
-class LoginVC: UIViewController {
+class ViewController: UIViewController {
 
-    override func viewDidLoad()
-    {
+    override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning()
-    {
+    override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 
-} // end class
+}
 
